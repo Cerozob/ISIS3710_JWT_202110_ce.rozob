0af5bf6e4bb1b9ff3de27671043baf937dc1b120
Cerozob <55302468+Cerozob@users.noreply.github.com>
2021-05-03 20:03:12 -0500

    funca

--- jwt/middleware.js
+++ jwt/middleware.js
@@ -1,5 +1,5 @@
 let jwt = require( 'jsonwebtoken' );
-const config = require( './config.js' );
+const config = require( './config' );
 
 // Función encargada de realizar la validación del token y que es directamente consumida por server.js
 let checkToken = ( req, res, next ) => {
